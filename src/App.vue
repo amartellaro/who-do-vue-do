6c70ebbadf9518a37158399faddd54051d4dea71
Alexandra Martellaro <alexandra.martellaro@slalom.com>
2019-04-05 12:51:45 -0500

    Header updates

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <Header/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="Who do? Vue do!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    Header,
-    HelloWorld
+    Header
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,29 @@
 <template>
-    <div><h1>Who do? Vue do!</h1></div>
+    <div class="header"><h1>{{ msg }}</h1></div>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <style scoped>
-    h1 {
-        color: #8585ad;
-    }
+.header {
+    width: 100%;
+    line-height: 50px;
+    background-color: #f0f0f5;
+    position: fixed;
+    top: 0;
+    margin: 0;
+}
+h1 {
+    color: #5c5c8a;
+    font-weight: 900;
+    font-size: 70px;
+    vertical-align: middle;
+}
 </style>
